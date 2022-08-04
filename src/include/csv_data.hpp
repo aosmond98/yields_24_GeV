153538e41e1d0ba330b865323ee680b441cd15e2
aosmond98 <aosmond@umich.edu>
2022-08-04 15:56:19 -0400

    for 24 GeV gen

--- src/include/csv_data.hpp
+++ src/include/csv_data.hpp
@@ -54,31 +54,24 @@ struct csv_data {
     // Make a string for the header of the csv file mPim case
     // 24 GeV test
     // return
-    // "w,q2,sf,elec_mom_rec,elec_th_rec,elec_phi_rec,prot_mom_mes,prot_theta_mes,prot_phi_mes,pip_mom_mes,pip_theta_"
-    //        "mes,pip_phi_mes,pim_mom_mes,pim_theta_mes,pim_phi_mes,mm2_mPim,mm2_mPip,mm2_mProt,mm2_exclusive_at_zero,energy_x_mu,"
-    //        "status_Pim,status_Pip,status_Prot,weight";
-    // return "w,q2,sfelec_th_rec,elec_phi_rec,weight,status_elec,status_Pim,status_Pip,status_Prot";
-    // return "w,q2,w_had,weight,energy_x_mu,mom_x_mu,w_mc,q2_mc";
-    // return "w,q2,w_had,weight,energy_x_mu,mom_x_mu";
 
     return "w_mc,q2_mc,weight";
-    // return
-    // "w_mc,q2_mc,elec_mom_gen,elec_th_gen,elec_phi_gen,prot_mom_gen,prot_th_gen,prot_phi_gen,pip_mom_gen,pip_th_"
-    //        "gen,pip_phi_gen,pim_mom_gen,pim_th_gen,pim_phi_gen,weight";
+
+
   }
 
   friend std ::ostream &operator<<(std::ostream &os, const csv_data &data) {
-    os << std::setprecision(7);
-     os << data.w << ",";
-     os << data.q2 << ",";
-     os << data.w_had << ",";
+    // os << std::setprecision(7);
+     // os << data.w << ",";
+     // os << data.q2 << ",";
+     // os << data.w_had << ",";
     // // os << data.w_diff << ",";
-     os << data.sf << ",";
+    // os << data.sf << ",";
     // // os << std::setprecision(10);
     // // os << data.elec_prime_m2 << ",";
     // // os << data.elec_m2 << ",";
     // // os << data.elec_energy_rec << ",";
-     os << data.elec_mom_rec << ",";
+    // os << data.elec_mom_rec << ",";
     // // os << data.elec_theta_rec << ",";
     // // os << data.elec_phi_rec << ",";
     // os << std::setprecision(8);
@@ -92,11 +85,11 @@ struct csv_data {
     // os << data.energy_x_mu << ",";
     // os << data.mom_x_mu << ",";
 
-    // os << data.w_mc << ",";
-    // os << data.q2_mc << ",";
+     os << data.w_mc << ",";
+    os << data.q2_mc << ",";
 
     // os << data.gen_elec_E << ",";
-    // os << data.gen_elec_mom << ",";
+   // os << data.gen_elec_mom << ",";
     // os << data.gen_elec_theta<< ",";
     // os << data.gen_elec_phi << ",";
 
@@ -111,7 +104,7 @@ struct csv_data {
     // os << data.gen_pim_mom << ",";
     // os << data.gen_pim_theta << ",";
     // os << data.gen_pim_phi<< ",";
-    os << std::setprecision(8);
+    // os << std::setprecision(8);
 
     os << data.weight_gen<< ",";
 
