0f65a196fdd51cdf8b16b5558819b63d9a29be07
aosmond98 <aosmond@umich.edu>
2022-10-02 18:02:37 -0400

    24gev rec round 394

--- src/exe/clas12_yields.hpp
+++ src/exe/clas12_yields.hpp
@@ -109,8 +109,8 @@ size_t run(std::shared_ptr<TChain> _chain, const std::shared_ptr<SyncFile>& _syn
      if (event->TwoPion_exclusive()) { // comment out for generated one
     // if (event->W() > 1.25 && event->W() < 2.55 && event->Q2() > 1.5 && event->Q2() < 10.5){
     // && abs(event->MM2_exclusive()) < 0.03 && abs(event->Energy_excl()) < 0.3) {
-     if (mc_event->W_mc() > 1.25 && mc_event->W_mc() < 2.55 && mc_event->Q2_mc() > 1.5 && mc_event->Q2_mc() < 30) { //added from email from krishna
-     }
+     if (mc_event->W_mc() > 1.25 && mc_event->W_mc() < 2.55 && mc_event->Q2_mc() > 1.5 && mc_event->Q2_mc() < 30) ; //added from email from krishna
+     
     //   //&&
     //   // abs(event->MM2_exclusive()) < 0.03) {
     //   // total++;
@@ -194,6 +194,6 @@ size_t run(std::shared_ptr<TChain> _chain, const std::shared_ptr<SyncFile>& _syn
  // std::cout << "Percent = " << 100.0 * total / num_of_events << std::endl;
   // Return the total number of events
  // return num_of_events;
-
+  }
 #endif
 
