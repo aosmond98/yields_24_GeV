6a19bb5bb5992f0dd2ab0528f885b8f0c62335b0
aosmond98 <aosmond@umich.edu>
2022-10-02 18:40:12 -0400

    please

--- src/exe/clas12_yields.hpp
+++ src/exe/clas12_yields.hpp
@@ -109,7 +109,7 @@ size_t run(std::shared_ptr<TChain> _chain, const std::shared_ptr<SyncFile>& _syn
      if (event->TwoPion_exclusive()) { // comment out for generated one
     // if (event->W() > 1.25 && event->W() < 2.55 && event->Q2() > 1.5 && event->Q2() < 10.5){
     // && abs(event->MM2_exclusive()) < 0.03 && abs(event->Energy_excl()) < 0.3) {
-     if (mc_event->W_mc() > 1.25 && mc_event->W_mc() < 2.55 && mc_event->Q2_mc() > 1.5 && mc_event->Q2_mc() < 30) {} //added from email from krishna
+     if (mc_event->W_mc() > 1.25 && mc_event->W_mc() < 2.55 && mc_event->Q2_mc() > 1.5 && mc_event->Q2_mc() < 30) { //added from email from krishna
      
     //   //&&
     //   // abs(event->MM2_exclusive()) < 0.03) {
