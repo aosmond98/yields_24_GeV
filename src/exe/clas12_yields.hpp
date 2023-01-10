449da95ccec5795f39758fbe593b3f92fcfd8021
aosmond98 <aosmond@umich.edu>
2023-01-09 22:05:05 -0500

    22 GeV

--- src/exe/clas12_yields.hpp
+++ src/exe/clas12_yields.hpp
@@ -16,11 +16,11 @@ size_t run(std::shared_ptr<TChain> _chain, const std::shared_ptr<SyncFile>& _syn
   // Get the number of events in this thread
   size_t num_of_events = (int)_chain->GetEntries();
 
-  float beam_energy = 24.0; // change this for 24.0 
+  float beam_energy = 22.0; // change this for 24.0 
   if (std::is_same<CutType, rga_Cuts>::value) {
-    beam_energy = 24.0;
+    beam_energy = 22.0;
   } else if (std::is_same<CutType, uconn_Cuts>::value) {
-    beam_energy = 24.0;
+    beam_energy = 22.0;
   }
 
   if (getenv("BEAM_E") != NULL) beam_energy = atof(getenv("BEAM_E"));
